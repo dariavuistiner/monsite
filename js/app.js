eca6294750fb90d1582936aea7a14cd96eaa9442
Sebastian Haag <SebastianHaag@MacBook-Pro-de-Sebastien.local>
2015-05-06 03:31:35 +0200

    fun figure element work out

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-$(document).foundation();
+$(document).ready(function() {
+   $(document).foundation({
+      reveal: {
+         animation: 'fade',
+      }
+   });
+   $(document).on('opened.fndtn.reveal', '[data-reveal]', function () {
+      $('html').addClass('modal-open');
+   });
+
+   $(document).on('closed.fndtn.reveal', '[data-reveal]', function () {
+      $('html').removeClass('modal-open');
+   });
+});
