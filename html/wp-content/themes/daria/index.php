fbde6570b5ed6656a8cd36897895041be3f5e923
Charlie Hadden <charlie@builtbybuffalo.com>
2015-08-16 19:19:45 +0100

    Homepage template

--- html/wp-content/themes/daria/index.php
+++ html/wp-content/themes/daria/index.php
@@ -1,109 +1,55 @@
 <?php get_header() ?>
 <nav role="navigation">
-   <div class="row">
-      <div class="small-12 large-6 columns nav">
-         <a href="#home" class="cmn-t-underline-lr">home</a>
-         <a href="#work" class="cmn-t-underline-lr">work</a>
-         <a href="#contact" class="cmn-t-underline-lr">contact</a>
-      </div>
-   </div>
+    <div class="row">
+        <div class="small-12 large-6 columns nav">
+            <a href="#home" class="cmn-t-underline-lr">home</a>
+            <a href="#work" class="cmn-t-underline-lr">work</a>
+            <a href="#contact" class="cmn-t-underline-lr">contact</a>
+        </div>
+    </div>
 </nav>
 <header id="home" class="slide">
-   <div class="row fullWith">
-      <div class="small-12 columns">
-         <h1>Hello,<br>I am a graphic designer, a story <br class="show-for-large-up">teller and a passionate cupcake <br class="show-for-large-up">baker. I am curently in <span>Switzerland</span><br class="show-for-large-up"> scribbling ideas and eating lots<br class="show-for-large-up"> of chocolate.</h1>
-      </div>
-   </div>
+    <div class="row fullWith">
+        <div class="small-12 columns">
+            <?php the_field('intro_text') ?>
+        </div>
+    </div>
 </header>
 <section id="work" class="slide">
     <div class="row fullWidth">
-        <div class="small-6 medium-4 columns portfolioThumb">
-            <a href="portfolioItem1.html" style="background-image: url('img/portfolioThumb1.png')">
-                <div class="portfolioInfo">
-                    <h3>Intrepid</h3>
-                    <p>Graphic design / branding</p>
-                </div>
-            </a>
-        </div>
-        <div class="small-6 medium-4 columns portfolioThumb">
-            <a href="portfolioItem1.html" style="background-image: url('img/portfolioThumb1.png')">
-                <div class="portfolioInfo">
-                    <h3>Intrepid</h3>
-                    <p>Graphic design / branding</p>
-                </div>
-            </a>
-        </div>
-        <div class="small-6 medium-4 columns portfolioThumb">
-            <a href="portfolioItem1.html" style="background-image: url('img/portfolioThumb1.png')">
-                <div class="portfolioInfo">
-                    <h3>Intrepid</h3>
-                    <p>Graphic design / branding</p>
-                </div>
-            </a>
-        </div>
-        <div class="small-6 medium-4 columns portfolioThumb">
-            <a href="portfolioItem1.html" style="background-image: url('img/portfolioThumb1.png')">
-                <div class="portfolioInfo">
-                    <h3>Intrepid</h3>
-                    <p>Graphic design / branding</p>
-                </div>
-            </a>
-        </div>
-        <div class="small-6 medium-4 columns portfolioThumb">
-            <a href="portfolioItem1.html" style="background-image: url('img/portfolioThumb1.png')">
-                <div class="portfolioInfo">
-                    <h3>Intrepid</h3>
-                    <p>Graphic design / branding</p>
-                </div>
-            </a>
-        </div>
-        <div class="small-6 medium-4 columns portfolioThumb">
-            <a href="portfolioItem1.html" style="background-image: url('img/portfolioThumb1.png')">
-                <div class="portfolioInfo">
-                    <h3>Intrepid</h3>
-                    <p>Graphic design / branding</p>
-                </div>
-            </a>
-        </div>
+        <?php $work = new wp_query('category_name=work&posts_per_page=6') ?>
+        <?php while ($work->have_posts()) : $work->the_post() ?>
+            <div class="small-6 medium-4 columns portfolioThumb">
+                <a href="<?php the_permalink() ?>" style="background-image: url('img/portfolioThumb1.png')">
+                    <div class="portfolioInfo">
+                        <h3><?php the_title() ?></h3>
+                        <p><?php the_field('skills') ?></p>
+                    </div>
+                </a>
+            </div>
+        <?php endwhile; the_post() ?>
     </div>
 </section>
 <footer id="contact">
-  <div class="row fullWidth">
-     <div class="small-12 medium-6 large-4 columns">
-        <div class="portrait"></div>
-     </div>
-     <div id="descCol" class="small-12 medium-6 large-4 columns">
-        <h2 class="uppercase">I am Daria, 21 years old, cat lover</h2>
-        <span></span>
-        <p>Currently in the summer before my third and last year of Art Direction’s degree, I  am an open minded person and creative mind always on the look for new experiences. A traveler, cupcake baker, runner and design lover. If you feel like seeing a few selfies or cakes I make, go there. Or if you’d rather just send me a love letter or new recipe you discovered, it’s here!</p>
-     </div>
-     <div id="cvCol" class="small-12 large-4 columns">
-        <p>
-           <strong class="cvPosition">2014 / Built By Buffalo</strong><br>
-           Web design / graphic design internship<br>
-           Brighton, England
-        </p>
-        <p>
-           <strong class="cvPosition">2014 / Digital District</strong><br>
-           Post production internship<br>
-           Paris, France
-        </p>
-        <p>
-           <strong class="cvPosition">2013 / Starting a bachelor</strong><br>
-           Art direction / graphic design<br>
-           Geneva, Switzerland
-        </p>
-        <p>
-           <strong class="cvPosition">2013 / Road trip on the West Coast</strong><br>
-           Photographing / Laughing / Backpacking<br>
-           California, USA
-        </p>
-        <p>
-           <strong class="cvPosition">2012 / Graduating from Highschool</strong><br>
-           Visual Arts / Lots of other stuff<br>
-           Morges, Switzerland
-        </p>
-     </div>
-  </div>
+    <div class="row fullWidth">
+        <div class="small-12 medium-6 large-4 columns">
+            <div class="portrait"></div>
+        </div>
+        <div id="descCol" class="small-12 medium-6 large-4 columns">
+        <h2 class="uppercase"><?php the_field('bio_heading') ?></h2>
+            <span></span>
+            <?php the_field('bio_body') ?>
+        </div>
+        <div id="cvCol" class="small-12 large-4 columns">
+            <?php $positions = new wp_query('category_name=cv-entry') ?>
+            <?php while ($positions->have_posts()) : $positions->the_post() ?>
+                <p>
+                    <strong class="cvPosition"><?php the_title() ?></strong><br>
+                    <?php the_field('skills') ?><br>
+                    <?php the_field('location') ?>
+                </p>
+            <?php endwhile ?>
+        </div>
+    </div>
 </footer>
 <?php get_footer() ?>
