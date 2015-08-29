99a1b1079ff0a3e5e2b178a246b53d8a3ac65623
Charlie Hadden <charlie@builtbybuffalo.com>
2015-08-29 21:54:12 +0100

    Separate grunt task for minified css

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,14 @@ module.exports = function(grunt) {
       options: {
         includePaths: ['html/vendor/foundation/scss']
       },
+      dev: {
+        options: {
+          outputStyle: 'expanded'
+        },
+        files: {
+          'html/css/app.css': 'scss/app.scss'
+        }
+      },
       dist: {
         options: {
           outputStyle: 'compressed'
@@ -34,6 +42,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('build', ['sass']);
+  grunt.registerTask('build', ['sass:dev']);
   grunt.registerTask('default', ['build','watch']);
 }
