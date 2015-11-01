32bcaef4f07b6ebf661a8476abc99416f91b6a84
Charlie Hadden <charlie@builtbybuffalo.com>
2015-11-01 11:38:01 +0000

    Page titles

--- html/wp-content/themes/daria/header.php
+++ html/wp-content/themes/daria/header.php
@@ -3,7 +3,11 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Daria!!!</title>
+    <?php if (is_front_page()): ?>
+        <title><?php echo bloginfo('name') ?> - <?php echo the_title() ?></title>
+    <?php else: ?>
+        <title><?php echo the_title() ?> - <?php echo bloginfo('name') ?></title>
+    <?php endif; ?>
     <link rel="stylesheet" href="css/app.css" />
     <script src="vendor/modernizr/modernizr.js"></script>
     <?php wp_head(); ?>
